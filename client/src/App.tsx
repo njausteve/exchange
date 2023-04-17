ee6b93d1ef20639a2a8c0bdcaed398b3f1b1680d
njausteve <njaustevedomino@gmail.com>
2023-04-17 21:43:41 +0800

    feat: initialize create react app
    Signed-off-by: njausteve <njaustevedomino@gmail.com>

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'antd/dist/reset.css';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      Render app here
+    </div>
+  );
+}
+
+export default App;
